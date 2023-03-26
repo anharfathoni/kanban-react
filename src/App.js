808ce753e20b5ab15cc573f3395cc6c907cc115f
Anhar Amir Fathoni <anhar.fathoni@graveltechnology.com>
2023-03-26 21:06:10 +0700

    setup kanban project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
+import Home from 'pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='register' element={<Register />} />
+      <Route path='login' element={<Login />} />
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
